e203ab04e37f976634bcac295d53755a18df766b
Ngoan Tran <ngoan98travinh@gmail.com>
2023-05-21 22:06:41 +0700

    add custom image loader

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,4 @@
 export const appConfig = {
   API_HOST: process.env.API_HOST!,
   FILE_HOST: process.env.FILE_HOST!,
-  FILE_PATH: process.env.FILE_PATH!,
 };
---
+++ src/utils/image-loader.ts
@@ -0,0 +1,10 @@
+import { appConfig } from "@/config/env";
+
+type Params = { src: string; width: number; quality: number };
+
+export default function customImageLoader({ src, width, quality }: Params) {
+  if (src.startsWith("/_next/static")) {
+    return src;
+  }
+  return `${appConfig.FILE_HOST}${src}?width=${width}&quality=${quality || 75}`;
+}
